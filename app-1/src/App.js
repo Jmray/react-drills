66e0afa1f52f186882e6065b3c355232766ce695
Justus Ray <justusmray@gmail.com>
2019-01-26 12:14:07 -0700

    finished app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,18 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      userInput: '',
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input className='inputBox' onChange={event => this.handleInputChange(event.target.value)}/>
+        <div className='displayBox'>{this.state.userInput}</div>
       </div>
     );
+  }
+  handleInputChange(value){
+    this.setState({
+      userInput: value,
+    })
   }
 }
 
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      greetingsAndGoodbyes: ['hello','goodbye','seeyoulater','salutations']
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h4>{this.state.greetingsAndGoodbyes[0]}</h4>
+        <h4>{this.state.greetingsAndGoodbyes[1]}</h4>
+        <h4>{this.state.greetingsAndGoodbyes[2]}</h4>
+        <h4>{this.state.greetingsAndGoodbyes[3]}</h4>
+
+        
       </div>
     );
   }

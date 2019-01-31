f6d6328e2ef811e9940dedbb1bf70a8435fadd0e
Justus Ray <justusmray@gmail.com>
2019-01-31 08:11:07 -0700

    this is app-3 being finished

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,19 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      
+      userInput: '',
+      list:['pakistan', 'qatar', 'croatia', 'tuvalu', 'korea', 'canada', 'greenland']
+    }
+  }
   render() {
+    let listToDisplay = this.state.list.filter( (element, index) => {
+      return element.includes( this.state.userInput );
+    }).map( (element, index) => {
+      return <h2 key={ index }>{ element }</h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input className='inputBox' value={this.userInput} onChange={event => this.handleChange(event.target.value)}/>
+      {listToDisplay}
+        
       </div>
     );
   }
-}
+  handleChange(value){
+    this.setState({
+      userInput: value,
+    })
+  }
+  
+  }
+
 
 export default App;

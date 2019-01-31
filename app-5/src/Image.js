ae52d4e96fbe00da24af9be581e435319bff5646
Justus Ray <justusmray@gmail.com>
2019-01-31 08:53:12 -0700

    this is app-5 completed

---
+++ app-5/src/Image.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+
+export default function Images (props){
+    return(
+        <div>
+            <img src={props.myImage} alt=''/>
+        </div>
+    )
+
+}
